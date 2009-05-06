35c1fd9cdec69faed258f676eec4dc62b6e6e99c
xyz20003@gmail.com <xyz20003@gmail.com@dbad078c-0a43-0410-a57f-b7fa355b1b6d>
2009-05-06 03:46:51 +0000

    升级依赖 felix-1.6.0 -> 1.6.1 maven-overview-plugin-1.3 -> 1.4

--- trunk/parent/child/clean.bat
+++
@@ -1,6 +0,0 @@
-del *.bak
-del *.class
-del pom.xml
-del *.txt
-rmdir /s /q target
-rmdir /s /q webapp
--- trunk/parent/child/run.bat
+++
@@ -1,8 +0,0 @@
-call clean
-
-javac Main.java
-java Main createChildPom
-
-call mvn dependency:tree -o > tree.txt
-
-java Main createJarsTxt
